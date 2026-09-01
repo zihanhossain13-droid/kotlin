974d1805a08b85715b683b3bee39cb804bbd1479
Zihan Hossain <zihanhossain13@gmail.com>
2026-09-01 09:41:36 +0600

    Add files via upload

---
+++ callback3.kt
@@ -0,0 +1,22 @@
+interface LoginCallback{
+  fun onSuccess()
+  fun onFailed()
+}
+fun login(username:String,callback:LoginCallback){
+  if(username=="Zihan"){
+    callback.onSuccess()
+  }
+  else{
+    callback.onFailed()
+  }
+}
+fun main(){
+  login("Zihan",object:LoginCallback{
+    override fun onSuccess(){
+      println("Login Success")
+    }
+    override fun onFailed(){
+      println("Login Failed")
+    }
+  })
+}
