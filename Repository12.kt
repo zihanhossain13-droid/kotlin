aaa7cb6f89c5976e68399ef44478337543318b1a
Zihan Hossain <zihanhossain13@gmail.com>
2026-09-02 11:47:02 +0600

    Add files via upload

---
+++ Repository12.kt
@@ -0,0 +1,32 @@
+class Repository<T>{
+  private var data=mutableListOf<T>()
+  fun save(value:T){
+data.add(value)
+  }
+  fun count():Int{
+    return data.size
+  }
+  fun clear(){
+    return data.clear()
+  }
+  fun isEmpty():Boolean{
+    if(data.isEmpty()){
+      return true
+    }else{
+      return false
+    }
+  }
+  
+  
+}
+fun main(){
+  val data=Repository<Int>()
+  data.save(100)
+  data.save(200)
+  data.save(300)
+  println(data.count())
+  data.clear()
+  println(data.count())
+  println(data.isEmpty())
+
+}
