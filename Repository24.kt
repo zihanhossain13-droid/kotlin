f1a76e6cf72b7bfe0822d19ae97311ba24e76af9
Zihan Hossain <zihanhossain13@gmail.com>
2026-09-02 16:31:48 +0600

    Add files via upload

---
+++ Repository24.kt
@@ -0,0 +1,21 @@
+data class User(
+  val name:String,
+  val age:Int
+)
+class Repository<T>{
+  private var data=mutableListOf<T>()
+  fun save(value:T){
+    data.add(value)
+
+  }
+  fun <R> map(transform:(T)->R):List<R>{
+    return data.map(transform)
+  }
+}
+fun main(){
+  val data=Repository<User>()
+  data.save(User("Zihan",20))
+  data.save(User("Rahim",25))
+  data.save(User("Karim",30))
+  println(data.map{"${it.name} is ${it.age} years old."})
+}
