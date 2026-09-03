833fe5781578d5b0fc5c2b102501c13ab4d82eeb
Zihan Hossain <zihanhossain13@gmail.com>
2026-09-03 21:50:57 +0600

    Add files via upload

---
+++ HOF23.kt
@@ -0,0 +1,17 @@
+data class User(
+  var name:String,
+  var age:Int
+)
+fun processUser(users:List<User>,condition:(User)->Boolean):List<User>{
+  return users.filter(condition)
+}
+fun main(){
+  val users = listOf(
+    User("Zihan", 20),
+    User("Rahim", 16),
+    User("Karim", 25),
+    User("Mahfuz", 15)
+)
+val result=processUser(users,{it.age>=18})
+println(result)
+}
