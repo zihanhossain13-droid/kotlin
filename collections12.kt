659014af6114478c3f5eda62250fa1ed00aadd42
Zihan Hossain <zihanhossain13@gmail.com>
2026-09-03 23:02:12 +0600

    Add files via upload

---
+++ collections12.kt
@@ -0,0 +1,8 @@
+fun hasEvenNumber(numbers: List<Int>): Boolean {
+    return numbers.any{it>0}
+}
+fun main(){
+  val numbers = listOf(5, 10, -3, 20, 15)
+  val result=hasEvenNumber(numbers)
+  println(result)
+}
