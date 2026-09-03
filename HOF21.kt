135104b56282356d17bc5d2995834f4bca86b5a3
Zihan Hossain <zihanhossain13@gmail.com>
2026-09-03 20:15:03 +0600

    Add files via upload

--- HOF21.kt
+++ HOF21.kt
@@ -1,8 +1,8 @@
 fun processNumber(number:List<Int>,operation:(Int)->Int):List<Int>{
-  return number.filter{it%2==0}.map(operation)
+  return number.filter{it>10}.map(operation)
 }
 fun main(){
-  val numbers = listOf(5, 10, 15, 20, 25, 30)
-  val result=processNumber(numbers){ it*2}
+  val numbers = listOf(3, 8, 12, 15, 20, 25)
+  val result=processNumber(numbers){it*2}
   println(result)
 }
