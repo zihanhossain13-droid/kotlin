78ee1af66d3f44032e75e3e31ec35cd76e1e7c29
Zihan Hossain <zihanhossain13@gmail.com>
2026-09-04 19:05:30 +0600

    Add files via upload

---
+++ with.kt
@@ -0,0 +1,15 @@
+data class User(
+    val name: String,
+    val age: Int
+)
+
+fun printUser(user: User) {
+    with(user) {
+      println(this.name)
+      println(this.age)
+    }
+}
+fun main(){
+  val result=printUser(User("Zihan",24))
+  
+}
