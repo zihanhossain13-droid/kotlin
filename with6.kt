a2c093a481cc0a679d42a11dd13e93386f4ef97a
Zihan Hossain <zihanhossain13@gmail.com>
2026-09-04 19:55:13 +0600

    Add files via upload

---
+++ with6.kt
@@ -0,0 +1,15 @@
+data class Student(
+    val name: String,
+    val math: Int,
+    val english: Int
+)
+
+fun getResult(student: Student): String {
+  return with(student){
+    "$name Total:${math+english},Average=${(math+english)/2.0}"
+  }
+}
+fun main(){
+  var with=getResult(Student("Zihan",80,80))
+  println(with)
+}
