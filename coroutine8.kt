89a224ca64b45ada79e5af1a05c3c253f8ad118a
Zihan Hossain <zihanhossain13@gmail.com>
2026-09-05 08:39:52 +0600

    Add files via upload

---
+++ coroutine8.kt
@@ -0,0 +1,14 @@
+import kotlinx.coroutines.*
+fun main()=runBlocking{
+    val result=async{
+        delay(1000)
+        10+30
+    }
+    val result2=async{
+        delay(2000)
+        20+30
+    }
+    println(result.await())
+    println(result2.await())
+    
+}
