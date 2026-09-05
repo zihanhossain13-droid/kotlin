d1c89c8f007f3a8201a6190aa6c2a35e95bca04c
Zihan Hossain <zihanhossain13@gmail.com>
2026-09-05 17:27:28 +0600

    Add files via upload

---
+++ coroutineExceptionHandler8.kt
@@ -0,0 +1,16 @@
+import kotlinx.coroutines.*
+fun main()=runBlocking{
+    val handler=CoroutineExceptionHandler{_,exception->
+    println("Error:${exception.message}")}
+    val scope=CoroutineScope(SupervisorJob()+handler)
+    scope.launch{
+        println("User Loaded")
+    }
+    scope.launch{
+        throw Exception("Network Error")
+    }
+    scope.launch{
+        println("Profile Loaded")
+    }
+    Thread.sleep(1000)
+}
