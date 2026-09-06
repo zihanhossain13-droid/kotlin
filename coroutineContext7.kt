6bbe7682664d0f73e0dbe9fba92117810c36229a
Zihan Hossain <zihanhossain13@gmail.com>
2026-09-06 08:45:42 +0600

    Add files via upload

---
+++ coroutineContext7.kt
@@ -0,0 +1,18 @@
+import kotlinx.coroutines.*
+
+fun main() {
+
+    val scope = CoroutineScope(
+        Dispatchers.Default + CoroutineName("ParentTask")
+    )
+
+    scope.launch {
+
+        launch {
+            println(coroutineContext[CoroutineName]?.name)
+            println(coroutineContext[CoroutineDispatcher])
+        }
+    }
+
+    Thread.sleep(1000)
+}
