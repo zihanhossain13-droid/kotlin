ef420c4b8ccfff9709ba50c6fbe9d5648529f382
Zihan Hossain <zihanhossain13@gmail.com>
2026-09-06 14:04:47 +0600

    Add files via upload

---
+++ coroutineContext29.kt
@@ -0,0 +1,17 @@
+import kotlinx.coroutines.*
+
+fun main() = runBlocking {
+
+    val scope = CoroutineScope(Job())
+
+    scope.launch {
+        throw Exception("Task 1 Failed")
+    }
+
+    scope.launch {
+        delay(1000)
+        println("Task 2 Done")
+    }
+
+    delay(2000)
+}
